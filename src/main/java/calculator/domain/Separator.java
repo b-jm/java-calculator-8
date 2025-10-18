114751a77fae19abb6a82759e762504b64c6132f
Jimin <bokjimin143@gmail.com>
2025-10-19 01:55:39 +0900

    feat: Separator 클래스 초기 설정
    기본 구분자(, :)를 DEFAULT_DELIMITERS 상수로 추가
    커스텀 구분자 인식을 위한 정규식 패턴(CUSTOM_PATTERN) 정의

--- src/main/java/calculator/domain/Separator.java
+++ src/main/java/calculator/domain/Separator.java
@@ -1,4 +1,10 @@
 package calculator.domain;
 
+import java.util.regex.Pattern;
+
 public class Separator {
+
+    private static final String DEFAULT_DELIMITERS = ",|:";
+    private static final Pattern CUSTOM_PATTERN = Pattern.compile("//(.+)\\n(.*)");
+    
 }
